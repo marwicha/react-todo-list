1698cc032946db66b21afa904f8bb436349c8fa4
marwa <marwa.rekik@esprit.tn>
2020-02-18 22:53:12 +0100

    feat(TodoInput): onChange event passing the props between 2 components

--- src/App.js
+++ src/App.js
@@ -7,12 +7,32 @@ import TodoInput from './components/TodoInput'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import uuid from 'uuid'
 class App extends Component {
+
+    state = {
+        items: [], 
+        id:0,
+        item:'',
+        editedItem: false
+    }
+
+    handleChange(e) {
+        this.setState({item : e.target.value})
+    }
+
   render() {
     return (
-        <div>
-        <h1> todo Input</h1>
-            <TodoInput/>
+        <div className="container">
+        <div className="row">
+        <div className="col-10 mx-auto col-md-8 mt-4">
+        <h1 className="text-capitalize text-center">
+        
+        Todo Input 
+        </h1>
+            <TodoInput item={this.state.item} handleChange={this.handleChange}/>
             <TodoList/>
+
+        </div>
+        </div>
         </div>
     )
 }
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
     render() {
+
+        const {item, handleChange} = this.props
+
         return (
             <div className="card card-body my-3">
                 
@@ -18,7 +21,12 @@ export default class TodoInput extends Component {
 
             </div>
 
-            <input type="text" className="form-control" placeholder="add a todo"/>
+            <input type="text" className="form-control"
+             placeholder="add a todo"
+             value={item}
+             onChange={handleChange}
+             />
+
             </div>
             <button type="submit" 
             className="btn btn-block btn-primary mt-3">
