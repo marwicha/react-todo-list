453ca1bc5b0ff62e7e455a70d312d8e5daa5d509
marwicha <marwa.rekik@esprit.tn>
2020-02-18 17:20:57 +0100

    chore(index.html): add font awesome and bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import TodoList from './components/TodoList'
 import TodoInput from './components/TodoInput'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import uuid from 'uuid'
 class App extends Component {
   render() {
     return (
